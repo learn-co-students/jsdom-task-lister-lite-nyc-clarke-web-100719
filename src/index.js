7b1c1546d1c425eec2d4c0620ddfc2162862fb20
TJBender <tiana.bender01@gmail.com>
2019-11-19 18:21:34 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
 });
+
+let formField = document.querySelector('#new-task-description')
+let form = document.querySelector("#create-task-form")
+
+//sort of capture listener on input field
+function updatedField() {
+  let input = document.querySelector('#new-task-description').value;
+  return input;
+}
+
+// adds things to toDo list
+function addToDo(toDo) {
+  let list = document.querySelector('#tasks');
+  list.innerHTML += `<li> ${toDo} </li>`
+}
+
+//need an event listening that does our addToDo function on submit
+//maybe works?
+form.addEventListener('submit', function (event) {
+  event.preventDefault();
+  addToDo(updatedField());
+})
