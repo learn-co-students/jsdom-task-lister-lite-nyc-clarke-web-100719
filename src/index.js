053a03eb22218f75f9d12067b992947485dde1aa
jastaffen <jack.staffen@gmail.com>
2019-11-26 18:52:40 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
 });
+
+const tasksList = document.getElementById('tasks');
+const form = document.getElementById('create-task-form');
+
+form.addEventListener('click', function(event){
+  event.preventDefault();
+  if (event.target.id === 'submit') {
+    let newToDo = document.getElementById('new-task-description');
+    tasksList.innerHTML += `<li>${newToDo.value}</li>`
+    form.reset();
+  }
+})
